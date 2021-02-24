cfcf4010f5c916b433d912fbc3bdd0174dee5ee0
dobbyjang0 <dobbyjang0@gmail.com>
2021-02-24 21:53:22 +0900

    start

---
+++ bot_main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 21:51:25 2021
+
+@author: dobbyjang
+"""
---
+++ stock.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 21:51:51 2021
+
+@author: dobbyjang
+"""
