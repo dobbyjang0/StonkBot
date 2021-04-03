364436e8074b6cecea77adf38e199fc36f38e26e
IncheonYSH <ysh121095@gmail.com>
2021-04-03 21:25:49 +0900

    Add market_data.py

--- market_data.py
+++ market_data.py
@@ -1,31 +1,41 @@
-from xing import xasession
-from xing import xacom
-from xing import xareal
-import xing
+from xing_api import XASession
+from xing_api import XAQuery
+from xing_api import XAReal
+import pandas as pd
 import json
 
+# 로그인
+def login():
+    file_path = "./xing_user2.json"
+    with open(file_path, "r") as json_file:
+        user = json.load(json_file)
+    session = XASession()
+    session.login(user)
+
+# 모든종목 정보
+# 데이터프레임으로
+def stock_name():
+    query = XAQuery()
+    in_field = {"gubun": '0'}
+    query.set_inblock('t8430', in_field)
+    query.request()
+    out_count = query.get_count('t8430OutBlock')
+
+    shcode = [query.get_outblock('t8430OutBlock', ["shcode"], i)["shcode"] for i in range(out_count)]
+    hname = [query.get_outblock('t8430OutBlock', ["hname"], i)["hname"] for i in range(out_count)]
+    gubun = [query.get_outblock('t8430OutBlock', ["gubun"], i)["gubun"] for i in range(out_count)]
+
+    data = {
+        "code": shcode,
+        "name": hname,
+        "market": gubun
+    }
+
+    df = pd.DataFrame(data, columns = ["code", "name", "market"])
+    return df
+
+def main():
+    login()
 
 
-server = {
-    "address" :"hts.ebestsec.co.kr",    # 서버주소
-    "port" : 20001, # 서버포트
-    "type" : 0  # 서버 타입
-}
 
-# user = {
-#     "id" : "id",   # 아이디
-#     "passwd" : "password",  # 비밀번호
-#     "account_passwd" : "OOOO",  # 계좌 비밀번호
-#     "certificate_passwd" : "OOOOOOOO"   # 공인인증서 비밀번호
-# }
-
-file_path = "./xing_user.json"
-with open(file_path, "r") as json_file:
-    user = json.load(json_file)
-
-session = xasession.Session()
-session.login(server, user)
-xacom.parseJstatus("1")
-xareal.Real
-
-session.logout()
